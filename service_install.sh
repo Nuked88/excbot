bcf24bf6982f92aec26b57c0e31c67849c9c405b
nuked88 <nuked8@gmail.com>
2018-01-21 19:07:44 +0100

    Fix

--- service_install.sh
+++ service_install.sh
@@ -3,7 +3,7 @@ echo "Installing Service..."
 cp ./service/excbot.sh /usr/local/bin/
 cp ./service/excbot.service /etc/systemd/system/excbot.service
 echo "Done!"
-sudo systemctl daemon reload
+sudo systemctl daemon-reload
 sudo systemctl enable excbot
 echo "Starting service!"
 sudo systemctl start excbot
